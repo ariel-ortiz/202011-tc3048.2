68c4307378e5cb464dd943260abcfd12c7bcf82e
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-05-20 20:44:54 -0500

    More examples on writing simple WebAssembly code

--- WebAssembly/example.wat
+++ WebAssembly/example.wat
@@ -1,6 +1,8 @@
 ;; Simple WebAssembly example.
 
 (module
+  (import "math" "sin" (func $sin (param f32) (result f32)))
+
   (func
     ;; Function signature
     (export "duplicate")
@@ -11,5 +13,20 @@
     local.get $x
     i32.const 2
     i32.mul
+  )
+
+  (func
+    (export "foo")
+    (param $a f32)
+    (param $b f32)
+    (result f32)
+
+    ;; $a * 3 + $sin($b)
+    local.get $a
+    f32.const 3
+    f32.mul
+    local.get $b
+    call $sin
+    f32.add
   )
 )
--- WebAssembly/execute.js
+++ WebAssembly/execute.js
@@ -4,13 +4,19 @@ const fileName = './example.wat';
 const fs = require('fs');
 const wabt = require('wabt')();
 
+const importArgs = {
+  math: {
+    sin: x => Math.sin(x)
+  }
+};
+
 async function getInstance() {
   try {
     const fileContent = fs.readFileSync(fileName, 'utf8');
     const wasmModule = wabt.parseWat(fileName, fileContent);
     const buffer = wasmModule.toBinary({}).buffer;
     const module = await WebAssembly.compile(buffer);
-    return WebAssembly.instantiate(module, {});
+    return WebAssembly.instantiate(module, importArgs);
   } catch (error) {
     console.log(error.message);
     process.exit(1);
@@ -22,6 +28,9 @@ async function main() {
 
   const duplicate = instance.exports.duplicate;
   console.log(duplicate(5));
+
+  const foo = instance.exports.foo;
+  console.log(foo(10.1, 5.2));
 }
 
 main();
